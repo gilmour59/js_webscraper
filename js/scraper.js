a8317d30f81ba13077a12cf64ce4cdfc22e58e28
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2020-01-15 16:57:22 +0800

    error still

--- js/scraper.js
+++ js/scraper.js
@@ -1,6 +1,48 @@
 const puppeteer = require('puppeteer');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-async function scrapeNames(url){
+async function scrapeTopComps(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const region_div = await page.$x('/html/body/div[1]/div[3]/ul[1]');
+    const rank_div = await page.$x('/html/body/div[1]/div[3]/ul[2]');
+
+    //if element is not found it will return [];
+    const regions = region_div.$('li a.nav-link');
+
+    //region: global, br, eune, euw, jp, kr, lan, las, na, oce, tr, ru
+    //rank: all, challenger, grandmaster, master, diamond, platinum, gold, silver, bronze, iron
+    rl.question('What Region: ', function(region_input){
+        rl.question('What Rank: ', function(rank_input){
+            
+            console.log(regions);
+        })
+    });
+
+    rl.on('line', (input) => {
+        if (input == 'close' || input == 'exit'){
+            rl.close();
+        }
+    });    
+
+    rl.on("close", function() {
+        console.log("mama mo!");
+        process.exit(0);
+    });
+
+    browser.close();
+}
+
+scrapeTopComps('https://lolchess.gg/leaderboards');
+
+//testing if scraper works
+async function scrapeTest(url){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
@@ -22,4 +64,4 @@ async function scrapeNames(url){
     browser.close();
 }
 
-scrapeNames('https://lolchess.gg/leaderboards?region=na&tier=diamond');
+//scrapeTest('https://lolchess.gg/leaderboards?region=na&tier=diamond');
