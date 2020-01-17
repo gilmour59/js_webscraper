c2e0ac9f921fd2dfff1912b4b34f6a5e8b9fafa2
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2020-01-17 16:33:12 +0800

    check for workaround instead of nested questions

--- js/scraper.js
+++ js/scraper.js
@@ -10,23 +10,41 @@ async function scrapeTopComps(url){
     const page = await browser.newPage();
     await page.goto(url);
 
-    const region_div = await page.$x('/html/body/div[1]/div[3]/ul[1]');
-    const rank_div = await page.$x('/html/body/div[1]/div[3]/ul[2]');
+    //the method elementHandle.$x() returns: <Promise<Array<ElementHandle>>></Promise>
+    const [region_div] = await page.$x('/html/body/div[1]/div[3]/ul[1]');
+    const [rank_div] = await page.$x('/html/body/div[1]/div[3]/ul[2]');
 
     //if element is not found it will return [];
-    const regions = region_div.$('li a.nav-link');
+    const regions = await region_div.$$('li a.nav-link');
 
-    //region: global, br, eune, euw, jp, kr, lan, las, na, oce, tr, ru
-    //rank: all, challenger, grandmaster, master, diamond, platinum, gold, silver, bronze, iron
-    rl.question('What Region: ', function(region_input){
-        rl.question('What Rank: ', function(rank_input){
-            
-            console.log(regions);
-        })
+    //store the regions from html list
+    var regions_array = [];
+
+    for (var i = 0; i < regions.length; i++){
+        var region_name = await regions[i].getProperty('textContent');
+        var region_text = await region_name.jsonValue();
+        regions_array.push(region_text.trim());
+    }
+
+    //region: global, br, eune, euw, jp, kr, lan, las, na, oce, tr, ru    
+    console.log('Regions: global, br, eune, euw, jp, kr, lan, las, na, oce, tr, ru');
+
+    //if no workaround is found. just nest.
+    rl.question('What Region: ', function(region_input){        
+        if ((regions_array.indexOf(region_input.trim().toUpperCase())) != -1) {
+            console.log('region is in array!')
+            console.log(regions_array.indexOf(region_input.trim().toUpperCase()));
+        }
+        rl.pause();
     });
 
+    //rank: all, challenger, grandmaster, master, diamond, platinum, gold, silver, bronze, iron
+    rl.question('What Rank: ', function(rank_input){    
+
+    })
+
     rl.on('line', (input) => {
-        if (input == 'close' || input == 'exit'){
+        if (input == 'close' || input == 'exit' || input == 'end'){
             rl.close();
         }
     });    
